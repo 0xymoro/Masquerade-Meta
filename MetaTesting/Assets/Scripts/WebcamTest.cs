977ce32da43c0efe4d959f34c347752dd7fc3144
Jerry Meng <zmeng90@stanford.edu>
2018-01-09 22:18:34 -0800

    Add initial Meta Unity, get webcam's every frame

---
+++ MetaTesting/Assets/Scripts/WebcamTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta;
+using UnityEngine.UI;
+
+public class WebcamTest : MonoBehaviour {
+    public WebCamTexture webcamTexture;
+    public RawImage rawimage;
+    // Use this for initialization
+    void Start () {
+        webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        rawimage.texture = webcamTexture;
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(webcamTexture.GetPixels());
+    }
+}
